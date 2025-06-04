7c5f0a92c35c295585f215504024f1995dd0f7c6
TheChupa <160878340+TheChupa@users.noreply.github.com>
2025-06-04 11:58:02 -0500

    attempted the extra challenge, will need to come back, other code is there

--- src/main/java/org/example/JavaCafe.java
+++ src/main/java/org/example/JavaCafe.java
@@ -10,6 +10,14 @@ import java.util.Scanner;
 public class JavaCafe {
     private Scanner scanner = new Scanner(System.in);
     public List<String> order = new ArrayList<String>();
+    public List<Number> prices = new ArrayList<Number>();
+    int total = 0;
+    public void updateTotal() {
+       Number total = 0.0;
+        for(Number price : prices) {
+
+        }
+    }
 
     /**
      * Runs the Java Cafe program. (Mauin menu)
@@ -43,19 +51,26 @@ public class JavaCafe {
     public void viewFoodMenu() {
         while(true) {
             System.out.println("Food Menu");
-            System.out.println("1. Sandwich");
-            System.out.println("2. Salad");
-            System.out.println("3. Soup");
-            System.out.println("4. Return to Main Menu");
+            System.out.println("1. Sandwich $5");
+            System.out.println("2. Salad $3");
+            System.out.println("3. Soup $2");
+            System.out.println("4. Dessert $4");
+            System.out.println("5. Return to Main Menu");
 
             String choice = scanner.nextLine();
             if (choice.equals("1")) {
                 order.add("Sandwich");
+                prices.add(5);
             } else if (choice.equals("2")) {
                 order.add("Salad");
+                prices.add(3);
             } else if (choice.equals("3")) {
                 order.add("Soup");
+                prices.add(2);
             } else if (choice.equals("4")) {
+                order.add("Dessert");
+                prices.add(4);
+            } else if (choice.equals("5")) {
                 break;
             } else {
                 System.out.println("Invalid choice. Please try again.");
@@ -69,18 +84,21 @@ public class JavaCafe {
     public void viewDrinkMenu() {
         while(true) {
             System.out.println("Drink Menu");
-            System.out.println("1. Coffee");
-            System.out.println("2. Tea");
-            System.out.println("3. Juice");
+            System.out.println("1. Coffee $1");
+            System.out.println("2. Tea $1");
+            System.out.println("3. Juice $3");
             System.out.println("4. Return to Main Menu");
 
             String choice = scanner.nextLine();
             if (choice.equals("1")) {
                 order.add("Coffee");
+                prices.add(1);
             } else if (choice.equals("2")) {
                 order.add("Tea");
+                prices.add(1);
             } else if (choice.equals("3")) {
                 order.add("Juice");
+                prices.add(3);
             } else if (choice.equals("4")) {
                 break;
             } else {
@@ -97,5 +115,6 @@ public class JavaCafe {
         for (String item : order) {
             System.out.println(item);
         }
+        System.out.println("$"+total +" is your total");
     }
 }
